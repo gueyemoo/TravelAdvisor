c964b29a53b90d3b9c98b3096ae84062291a73e4
gueyemoo <m.gueye0411@gmail.com>
2021-08-22 19:51:15 +0200

    Setting up API request and fetching data

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+export const getPlacesData = async (sw, ne) => {
+    try {
+        //request
+        const { data: { data } } = await axios.get(URL, {
+            url: URL,
+            params: {
+                bl_latitude: sw.lat,
+                tr_latitude: ne.lat,
+                bl_longitude: sw.lng,
+                tr_longitude: ne.lng,
+            },
+            headers: {
+                'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+                'x-rapidapi-key': '863148eb4cmsh8f6b136b492f6a7p1e2642jsnd6eeb5142736'
+            }
+        });
+        
+        return data;
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,21 +7,25 @@ import Rating from '@material-ui/lab/Rating';
 // import mapStyles from '../../mapStyles';
 import useStyles from './styles.js';
 
-const Map = () => {
+const Map = ( {setCoordinates, setBounds, coordinates}) => {
     const classes = useStyles();
-    const matches = useMediaQuery('(min-width:600px)');
+    const isMobile = useMediaQuery('(min-width:600px)');
     
-    const coordinates = { lat:0 , lng: 0};
+        // const coordinates = { lat:0 , lng: 0};
 
     return (
         <div className={classes.mapContainer}>
             <GoogleMapReact
             bootstrapURLKeys= {{ key: 'AIzaSyCemYcZJjTq96Auzfw-jtGsZP-TBkEgCzA'}}
             defaultCenter={coordinates}
+            center={coordinates}
             defaultZoom={14}
             margin={[50, 50, 50, 50]}
             options={''}
-            onChange={''}
+            onChange={(e)=> {
+                setCoordinates({ lat: e.center.lat, lng: e.center.lng});
+                setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+            }}
             onChildClick={''}
             >
 
