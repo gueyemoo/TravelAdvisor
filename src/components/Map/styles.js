0d2fbd296a8021d28787bcd20041d4b908ffd447
gueyemoo <m.gueye0411@gmail.com>
2021-08-24 03:10:15 +0200

    Map fix on mobile

--- src/components/Map/styles.js
+++ src/components/Map/styles.js
@@ -5,7 +5,7 @@ export default makeStyles(() => ({
         padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100px', border:'1px solid black',
     },
     mapContainer: {
-        height: '90vh', width: '100%',
+        height: '90vh', width: '100%', padding:'3vh'
     },
     markerContainer: {
         position: 'absolute', transform: 'translate(-50%, -50%)', zIndex: 1, '&:hover': { zIndex: 2 }, 
